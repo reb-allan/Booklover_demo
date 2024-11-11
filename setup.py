0ace4ca493b0349627d577857dc193df7adca174
Your Name <tkz5ry@virginia.edu>
2024-11-10 22:52:11 -0500

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='M9HW',
+   version = '0.1',
+   description = 'Booklover HW for M9',
+   author = 'Rebekah Allan',
+   author_email = 'tkz5ry@virginia.edu',
+   license = 'MIT',
+   packages = ['HW']
+)
